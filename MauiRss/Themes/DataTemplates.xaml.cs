23fb53d3b2387c2aac160c36b2948bfd6296da00
Tim Miller <innerlogic4321@gmail.com>
2021-09-10 10:31:00 -0400

    Add Comments

--- MauiRss/Themes/DataTemplates.xaml.cs
+++ MauiRss/Themes/DataTemplates.xaml.cs
@@ -1,14 +1,24 @@
-﻿using Microsoft.Maui.Controls;
+﻿// <copyright file="DataTemplates.xaml.cs" company="Drastic Actions">
+// Copyright (c) Drastic Actions. All rights reserved.
+// </copyright>
+
+using Microsoft.Maui.Controls;
 using Microsoft.Maui.Controls.Xaml;
 
 namespace MauiRss.Themes
 {
+    /// <summary>
+    /// Data Templates.
+    /// </summary>
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class DataTemplates : ResourceDictionary
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DataTemplates"/> class.
+        /// </summary>
         public DataTemplates()
         {
-            InitializeComponent();
+            this.InitializeComponent();
         }
     }
 }
